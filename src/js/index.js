0da6f52bbda2db5f51490b828e52fe2b34915c4a
Павел Горбунов <pavelgorbunov@MacBook-Air-Pavel.local>
2025-01-29 13:21:02 +0400

    v. 0.0.1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import '../scss/style.scss'
+import javascriptLogo from '../svg/javascript.svg'
+import viteLogo from '/images/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
